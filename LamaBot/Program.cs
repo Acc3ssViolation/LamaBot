e141ef97b69c435a505f3242c4f91c1ec09dee52
Acc3ss Violation <wrdbrookhuis@gmail.com>
2024-03-19 19:35:07 +0100

    Add project files.

---
+++ LamaBot/Program.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+
+namespace LamaBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Discord.WebSocket.DiscordSocketClient();
+            client.
+            
+        }
+
+        private static Task DiscordLog(LogMessage message)
+        {
+            Console.WriteLine(message);
+            return Task.CompletedTask;
+        }
+    }
+}
