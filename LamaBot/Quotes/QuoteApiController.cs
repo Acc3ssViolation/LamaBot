980689f7786835a41373f8b2b76e7c3671b5c9b6
Acc3ss Violation <wrdbrookhuis@gmail.com>
2024-04-01 22:28:51 +0200

    Resolve user and channel references in quote API

--- LamaBot/Quotes/QuoteApiController.cs
+++ LamaBot/Quotes/QuoteApiController.cs
@@ -1,14 +1,16 @@
-﻿using LamaBot.Servers;
+﻿using Discord.WebSocket;
+using LamaBot.Servers;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace LamaBot.Quotes
 {
     [ApiController]
     [Route("api/v1/quote")]
-    public class QuoteApiController : ControllerBase
+    public partial class QuoteApiController : ControllerBase
     {
         [HttpGet("{guildId}")]
-        public async Task<IActionResult> GetQuotesAsync([FromServices] IQuoteRepository quoteRepository, [FromServices] IDiscordFacade discord, [FromServices] IServerSettings serverSettings, [FromRoute] ulong guildId, [FromQuery] string key, [FromQuery] bool resolveUsers = true)
+        public async Task<IActionResult> GetQuotesAsync([FromServices] IQuoteRepository quoteRepository, [FromServices] IDiscordFacade discord, [FromServices] IServerSettings serverSettings, [FromRoute] ulong guildId, [FromQuery] string key)
         {
             var requiredKey = await serverSettings.GetSettingAsync(guildId, QuoteSettings.QuoteApiKey);
             if (requiredKey == null || requiredKey != key)
@@ -16,22 +18,50 @@ namespace LamaBot.Quotes
 
             var quotes = await quoteRepository.GetQuotesAsync(guildId, HttpContext.RequestAborted);
 
-            if (resolveUsers)
+            var client = discord.Client;
+            for (var i = 0; i < quotes.Count; i++)
             {
-                var client = discord.Client;
-                for (var i = 0; i < quotes.Count; i++)
-                {
-                    var quote = quotes[i];
-                    if (quote.UserId == 0)
-                        continue;
+                var quote = quotes[i];
+                if (quote.UserId == 0)
+                    continue;
 
-                    var user = client.GetGuild(quote.GuildId)?.GetUser(quote.UserId);
-                    if (user != null)
-                        quotes[i] = quote with { UserName = user.DisplayName };
-                }
+                quotes[i] = ResolveReferences(quote, client);
             }
 
             return new JsonResult(quotes);
         }
+
+        private static Quote ResolveReferences(Quote quote, DiscordSocketClient client)
+        {
+            var guild = client.GetGuild(quote.GuildId);
+            if (guild == null)
+                return quote;
+
+            var content = UserRefRegex().Replace(quote.Content, (match) =>
+            {
+                var userId = ulong.Parse(match.Groups[1].ValueSpan);
+                var user = guild.GetUser(userId);
+                if (user != null)
+                    return $"@{user.DisplayName}";
+                return match.Value;
+            });
+            content = ChannelRefRegex().Replace(content, (match) =>
+            {
+                var channelId = ulong.Parse(match.Groups[1].ValueSpan);
+                var channel = guild.GetChannel(channelId);
+                if (channel != null)
+                    return $"#{channel.Name}";
+                return match.Value;
+            });
+
+            var user = guild.GetUser(quote.UserId);
+            var username = user?.DisplayName ?? quote.UserName;
+            return quote with { Content = content, UserName = username };
+        }
+
+        [GeneratedRegex("<@(\\d+)>")]
+        private static partial Regex UserRefRegex();
+        [GeneratedRegex("<#(\\d+)>")]
+        private static partial Regex ChannelRefRegex();
     }
 }
