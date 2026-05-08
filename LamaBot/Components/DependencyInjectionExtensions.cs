c2f8a0deb18e3e52f9d64fe48a465cdfaec0cb36
Acc3ss Violation <wrdbrookhuis@gmail.com>
2026-05-08 22:24:19 +0200

    Clean up DI

---
+++ LamaBot/Components/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LamaBot.Components
+{
+    internal static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddInteractiveComponents(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .AddSingleton<InteractiveComponentService>()
+                .AddSingleton<IHostedService>(sp => sp.GetRequiredService<InteractiveComponentService>())
+                .AddSingleton<IInteractiveComponentService>(sp => sp.GetRequiredService<InteractiveComponentService>());
+        }
+    }
+}
---
+++ LamaBot/Modules/CopyCat/DependencyInjectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LamaBot.Modules.CopyCat
+{
+    internal static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddCopyCat(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .AddSingleton<ITextMessageHandler, CopyCatMessageHandler>();
+        }
+    }
+}
